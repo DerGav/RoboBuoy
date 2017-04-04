e6c4368133d67001280fd2fba1cb6d036ae9671d
DerGav <gavin.gabriel@web.de>
2017-04-03 18:24:17 -0700

    'Add Targets...' Dialog pretty much functional now. Values are saved to a quick access array which is accessed by the target select element, and to a saving array which is intended to be sent to the server for saving in future.
    TODO: reload the possible groups on second opening of add targets dialog

--- roboBuoy_code_v2(python_webserver)/static/roboBuoy.js
+++ roboBuoy_code_v2(python_webserver)/static/roboBuoy.js
@@ -3,6 +3,7 @@
 //will in future also send commands to RoboBuoy
 
 var target_destinations = [];
+var target_destinations_with_groups = {};
 
 $(document).ready(function () { // execute as soon as page (=document) is loaded
 
@@ -146,6 +147,7 @@ function add_targetDestinations(targets)
 {
   //store index that coordinates will have in target_destinations array
   var target_destinations_index = 0;
+  target_destinations_with_groups = targets;
 
   //loop through the targets array
   for (var i=0; i < targets.length; i++)
@@ -215,61 +217,144 @@ function get_received_data()
 
 function updateTargets()
 {
-  //read values from form
+  // read values from form
   var name  = $('#location_name').val();
   var lat   = $('#location_lat' ).val();
   var long  = $('#location_long').val();
 
+  // user can set the group in 2 different ways so this is more complicated
   var group;
+
+  // get the value of the 'radio' select form element
   var existing_group = $('input:radio[name=new_or_existing_group]:checked').val();
+  // set variable depending on value
   if(existing_group === 'existing_group')
   {
+    // remember js has no typ binding, so this may look weird but it works!
     existing_group = true;
   } else {
     existing_group = false;
   }
+
+  // if we're adding to an existing group do this...
   if(existing_group)
   {
-    console.log('here');
+    // set group to the value selected by the select element
     group = $('#select_group').val();
 
+    //check to see that a real value was selected
     if(group === 'Choose Group...')
     {
       console.log('No Group Selected!');
+      // TODO: throw an error here or write sth to the UI
+      // console log not good enough
     }
   }
+  // if we're creating a new group..
   else {
+    //simply get the value of the new group name input field
     group = $('#new_group_name').val();
   }
 
-  //console.log(name,lat,long,group,existing_group);
-
-  // check if values were entered
+  // check if values were entered and are valid
+  // TODO: add sanity checks here, possibly check for duplicates as well...
   if(name && lat && long && group)
   {
+    // create new target object
+    var new_target = {
+      target_name: name,
+      Lat: lat,
+      Long: long
+    };
+
+    // load the target select UI element, we'll need it a few times
+    // no need to let jquery look for it again and again!
     var $targets = $('#targets');
+
+    // are we adding to a group?
     if(existing_group)
     {
-      $.each($targets.find('optgroup'), function () {
-        if ($(this).attr('label') === group)
+      // need this to calculate index in the quick access array
+      var array_index = 0;
+
+      // add new target to the saving array
+      // loop through saving array to find the righ spot
+      for(var i=0; i < target_destinations_with_groups.length; i++)
+      {
+        // add amount of coordinates per group to array_index, because
+        // they are all in one sequence in the quick access array
+        array_index += target_destinations_with_groups[i].coordinates.length;
+
+        // check if this is our group
+        if(target_destinations_with_groups[i].group_name === group)
         {
-          $(this).append(
-            $('<option></option>').html(name)
-          );
+          // add the new target object at the end of the saving array
+          target_destinations_with_groups[i].coordinates.push(new_target);
+          break;
+        }
+      }
+
+      // insert new target into the quick acces array
+      target_destinations.splice(array_index, 0, new_target);
+
+      // add new target to UI
+
+      // store if we have already inserted new element
+      var inserted = false;
+      //loop over all groups
+      $.each($targets.find('optgroup'), function () {
+
+        if(inserted) // if we have already inserted the new target..
+        {
+          //update the 'value' to resemble the index in the quick acces array
+          $.each($(this).find('option'), function () {
+            // ++variable increments the variable before it is evaluated
+            // so we set 'value' to 'array_index + 1'
+            $(this).attr('value', ++array_index);
+          });
+        }
+        // if we haven't inserted new target yet work on that
+        else {
+          // if the optgroup label equals 'group' that's were we have to insert
+          if ($(this).attr('label') === group)
+          {
+            // append the new option in this group
+            $(this).append(
+              $('<option></option>').attr('value',array_index)
+                                    .html(name)
+            );
+            // set the flag to to true because now we have inserted the target
+            inserted = true;
+          }
         }
       });
     }
+    // if we're creating a new group as well, do this...
     else {
+      // add new target to the end of the saving array
+      target_destinations_with_groups.push({
+        group_name: group,
+        coordinates: [new_target]
+      });
+
+      // add new target to the UI
+      // in this case we can just append the group and the new target
       $targets.append(
         $('<optgroup></optgroup>').attr('label', group)
                                   .append(
-                                    $('<option></option>').html(name)
+                                    $('<option></option>').attr('value', target_destinations.length)
+                                                          .html(name)
                                   )
       );
+
+      // add the new target to the quick acces array
+      target_destinations.push(new_target);
     }
   }
 
-  //reset all form fields to defaults
-  $('#new_target_form')[0].reset();
+  console.log(target_destinations);
+  console.log(target_destinations_with_groups);
 
+  // reset all form fields to defaults
+  $('#new_target_form')[0].reset();
 }
